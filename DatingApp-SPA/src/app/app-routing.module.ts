f92c81535fb6b76df512c2e4a7e36443308c176b
Tomas Kneitas <nosheep6@gmail.com>
2019-03-08 00:37:59 +0200

    theming

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -11,20 +11,23 @@ const routes: Routes = [
 
   {
     path: '',
-    component: HomeComponent
+    component: HomeComponent,
+    children: [
+      { path: 'register', component: RegisterComponent },
+    ]
   },
   {
     path: '',
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'register', component: RegisterComponent },
       { path: 'members', component: MemberListComponent},
       { path: 'lists', component: ListsComponent},
       { path: 'messages', component: MessagesComponent},
       { path: '**', redirectTo: '', pathMatch: 'full' }
     ]
-  }
+  },
+
 
   // { path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
   // { path: 'lists', component: ListsComponent, canActivate: [AuthGuard] },
